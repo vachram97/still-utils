ded6fbbdc6165bef6944f698ebf169f7615c92bc
Egor Marin <marin@phystech.edu>
2020-02-07 15:53:31 +0100

    Add extract_cell.py to extract provided cell info

---
+++ extract_cell.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+helpstring = """\
+Usage: ./extract_cell.py my.stream"""
+
+if sys.argv[1] in ["-h", "--help"]:
+    print(helpstring)
+    exit(0)
+elif not os.path.exists(sys.argv[1]):
+    print(f"File {sys.argv[1]} does not exist, check path or access rights")
+else:
+    cell_start = False
+    for line in open(sys.argv[1]):
+        if "Begin unit cell" in line:
+            cell_start = True
+            continue
+        elif "End unit cell" in line:
+            break
+        else:
+            if cell_start and line[0] != ";":
+                print(line, end="")
