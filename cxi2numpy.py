9a89f788f79fba1d2a189ba59008fa6b51810753
Egor Marin <marin@phystech.edu>
2020-03-02 17:06:22 +0300

    Start drafting main extracting function

--- cxi2numpy.py
+++ cxi2numpy.py
@@ -4,34 +4,10 @@ import h5py
 import numpy as np
 from itertools import chain, product
 import matplotlib.pyplot as plt
+from typing import Dict
 
 
-def extract_hits(
-    cxi_filename: str, indices: list, cxi_path="entry_1/data_1/data", **args,
-) -> np.ndarray:
-    """
-    From single cxi file and list of indices within it (which are usually "Event" field in crystfel stream), returns np.ndarray representing each hit image
-
-    Arguments:
-        cxi_filename {str} -- input cxi filename
-        cxi_path {str} -- path to data in hierarchical cxi structure
-        indices {list} -- indices of hit images along time coordinate
-
-    Returns:
-        np.ndarray -- (n_images, xsize, ysize), containing hits only
-    """
-
-    data = []
-
-    with h5py.File(cxi_filename, "r") as f:
-        for idx in indices:
-            current_nparray = np.array(f[cxi_path][idx])
-            data.append(current_nparray)
-
-    return np.array(data)
-
-
-def peaks_from_ndarray(
+def _peaks_from_ndarray(
     nparr: np.ndarray, size=1
 ) -> [np.ndarray, np.ndarray, np.ndarray]:
     """
@@ -54,10 +30,9 @@ def peaks_from_ndarray(
         ret += (np.roll(nparr, roll_combination, axis=(-2, -1)) < nparr).astype(np.int8)
 
     return np.nonzero(ret - num_neighbours > 0)
-    # return ret-num_neighbours, np.nonzero(ret - num_neighbours > 0)
 
 
-def peak_profiles(nparray: np.ndarray, size=3, start=10000, top=100) -> list:
+def peak_profiles(nparray: np.ndarray, size_compare=1, size_return=3) -> list:
     """
     Returns peak profiles as (n, x, y) tensor, with n=nparray.shape[0], and x=y=2*size+1
     
@@ -68,16 +43,48 @@ def peak_profiles(nparray: np.ndarray, size=3, start=10000, top=100) -> list:
         list -- peak profiles
     """
 
-    pks = peaks_from_ndarray(nparray)
+    pks = _peaks_from_ndarray(nparray, size=size_compare)
     answ = []
 
-    cnt = 0
     if len(nparray.shape) == 3:
         for n, x, y in zip(*pks):
-            cnt += 1
-            if cnt < start:
-                continue
-            if cnt == top + start:
-                return answ
-            profile = nparray[n][x - size : x + size + 1, y - size : y + size + 1]
+            profile = nparray[n][
+                x - size_return : x + size_return + 1,
+                y - size_return : y + size_return + 1,
+            ]
             answ.append(profile)
+
+    return answ
+
+
+def extract_hits(
+    cxi_filename: str,
+    indices: list,
+    cxi_path="entry_1/data_1/data",
+    size_compare=1,
+    size_return=5,
+) -> Dict:
+    """
+    From single cxi file and list of indices within it (which are usually "Event" field in crystfel stream), returns np.ndarray representing each hit image
+
+    Arguments:
+        cxi_filename {str} -- input cxi filename
+        cxi_path {str} -- path to data in hierarchical cxi structure
+        indices {list} -- indices of hit images along time coordinate
+        size_compare {int} -- peaks should be larger or equal to all peaks within (-size,+size) square
+        size_return {int} -- will return (2*size_return-1)**2 sized square with peak in center
+
+    Returns:
+        Dict -- dictionary of f'{image_filename|event : {'x':x, 'y':y, 'profile':np.ndarray}}
+    """
+
+    hits = {}
+    answ = {}
+
+    with h5py.File(cxi_filename, "r") as f:
+        for idx in indices:
+            current_key = (cxi_filename, idx)
+            current_nparray = np.array(f[cxi_path][idx])
+            hits[current_key] = current_nparray
+
+            peaks = peak_profiles(hits, size_compare=size_compare, size_return=size_return)
