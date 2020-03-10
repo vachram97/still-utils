3e37466471b34413f6e5c821c067ef658ef6639b
Egor Marin <marin@phystech.edu>
2020-03-10 20:33:10 +0300

    Build end-to-end pipeline with two launches: first to plot, second to run estimation. Add several parameters (plot, center_fs, center_ss, rmin, rmax, nbins)

--- json2center.py
+++ json2center.py
@@ -5,7 +5,7 @@ import json
 import sys
 import argparse
 from tqdm import tqdm
-from typing import List
+from typing import List, Tuple
 from itertools import combinations
 import matplotlib.pyplot as plt
 import warnings
@@ -14,37 +14,39 @@ import warnings
 def moving_average(arr: np.ndarray, window_size: int) -> np.ndarray:
     """
     Return moving average of 1D array in numpy
-    
+
     Arguments:
         arr {np.ndarray} -- input array
         window_size {int} -- window size
-    
+
     Returns:
         ret {np.ndarray} -- smoothened array
     """
     ret = np.cumsum(arr, dtype=float)
     ret[window_size:] = ret[window_size:] - ret[:-window_size]
-    return ret[window_size - 1:] / window_size
+    return ret[window_size - 1 :] / window_size
 
 
 def rms_mask(arr: np.ndarray, window_size: int, percentile=0.1) -> np.ndarray:
     """
     Return mask where arr's deviation is smaller than it's mean*percentile:
     RMSD[arr]_window_size < MEAN[arr]*percentile
-    
+
     Arguments:
         arr {np.ndarray} -- input array
         window_size {int} -- window size
-    
+
     Returns:
         np.ndarray -- boolean mask
     """
     arr_mean = moving_average(arr, window_size)
     arr_rmsd = np.power(moving_average(np.power(arr, 2), window_size), 0.5)
-    return np.where(np.abs(arr_rmsd) < np.abs(arr_mean)*percentile)
+    return np.where(np.abs(arr_rmsd) < np.abs(arr_mean) * percentile)
 
 
-def radial_binning(fs: np.ndarray, ss: np.ndarray, rs: np.ndarray, N=1000) -> np.ndarray:
+def radial_binning(
+    fs: np.ndarray, ss: np.ndarray, rs: np.ndarray, N=1000
+) -> np.ndarray:
     """
     Returns binned by radius table
 
@@ -62,7 +64,9 @@ def radial_binning(fs: np.ndarray, ss: np.ndarray, rs: np.ndarray, N=1000) -> np
     rmin, rmax = rs.min(), rs.max()
     step = (rmax - rmin) / N
     answ = []
-    for rcur, _ in tqdm(enumerate(np.linspace(rmin, rmax, N)), desc='Binning values', total=N):
+    for rcur, _ in tqdm(
+        enumerate(np.linspace(rmin, rmax, N)), desc="Binning values", total=N
+    ):
         mask = (rs < rcur + step) & (rs >= rcur)
         if sum(mask) > 0:
             rmean = rs[mask].mean()
@@ -97,11 +101,40 @@ def is_inside(triangle: np.ndarray, point: np.ndarray) -> bool:
     oa, ob, oc = a - point, b - point, c - point
     return (
         np.abs(
-            sum([ang(v1, v2)
-                 for v1, v2 in combinations([oa, ob, oc], 2)]) - 2 * np.pi
+            sum([ang(v1, v2) for v1, v2 in combinations([oa, ob, oc], 2)]) - 2 * np.pi
         )
         < 1e-2
     )
+
+
+def get_center_position_from_binning(
+    binning: np.ndarray, rmin=None, rmax=None
+) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+    """
+    Estimate center position from binning results
+
+    Arguments:
+        binning {np.ndarray} -- input (N,4) shape array
+
+    Keyword Arguments:
+        rmin {[type]} -- lower radius threshold (default: {None})
+        rmax {[type]} -- upper radius threshold (default: {None})
+
+    Returns:
+        Tuple[Tuple[float, float], Tuple[float, float]] -- (fs, ss) tuple
+    """
+    rs, num, fs, ss = binning.T
+
+    if rmin is None:
+        rmin = float("-inf")
+    if rmax is None:
+        rmax = float("inf")
+
+    mask = (rs >= rmin) & (rs < rmax)
+    fs_val, fs_std = fs[mask].mean(), fs[mask].std()
+    ss_val, ss_std = ss[mask].mean(), ss[mask].std()
+
+    return (fs_val, fs_std), (ss_val, ss_std)
 
 
 def circle(points: np.ndarray, acute_angle=True, presumable_centre=None) -> np.ndarray:
@@ -167,6 +200,30 @@ def main(args: List[str]):
     parser.add_argument(
         "input_json", type=str, help="input json file, produced by streampeaks2json.py",
     )
+    parser.add_argument(
+        "--rmin", type=int, default=None, help="Minimum radius for center estimation"
+    )
+    parser.add_argument(
+        "--rmax", type=int, default=None, help="Maximum radius for center estimation"
+    )
+    parser.add_argument(
+        "--center_fs",
+        type=int,
+        default=None,
+        help="Presumable center fs for acute angle rejection",
+    )
+    parser.add_argument(
+        "--center_ss",
+        type=int,
+        default=None,
+        help="Presumable center ss for acute angle rejection",
+    )
+    parser.add_argument(
+        "--plot", help="Whether to save plots or not", action="store_true",
+    )
+    parser.add_argument(
+        "--nbins", type=int, default=1000, help="Number of bins for radial binning"
+    )
 
     args = parser.parse_args()
 
@@ -177,8 +234,10 @@ def main(args: List[str]):
 
     points = np.array([(elem["fs"], elem["ss"]) for elem in peaks.values()])
 
-    # presumable_centre = points.mean(axis=0)
-    presumable_centre = np.array([719, 711])
+    if args.center_fs is not None and args.center_ss is not None:
+        presumable_centre = np.array([args.center_fs, args.center_ss])
+    else:
+        presumable_centre = None
     answ = []
 
     for _ in tqdm(range(args.iterations), desc="Sampling points"):
@@ -189,11 +248,21 @@ def main(args: List[str]):
             answ.append(cur)
 
     fs, ss, rs = np.array(answ).T
+    bins = radial_binning(fs, ss, rs, N=args.nbins)
+    if args.plot:
+        plt.plot(bins.T[0], bins.T[2], label="fs")
+        plt.plot(bins.T[0], bins.T[3], label="ss")
+        plt.xlim(args.rmin, args.rmax)
+        plt.savefig(f"{args.input_json}_plot.png", dpi=300)
+        print(f"Saved {args.input_json}_plot.png")
 
-    return radial_binning(fs, ss, rs)
+    (fs, fs_s), (ss, ss_s) = get_center_position_from_binning(
+        bins, rmin=args.rmin, rmax=args.rmax
+    )
+    print(f"fs: {fs} +- {fs_s}; ss: {ss} +- {ss_s}")
 
 
 if __name__ == "__main__":
     if not sys.warnoptions:
         warnings.simplefilter("ignore")
-    points = main(sys.argv[1:])
+    main(sys.argv[1:])
