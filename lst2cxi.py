bdea723e748c5d35994e61a2d805fb62b6089dc3
Egor Marin <marin@phystech.edu>
2020-05-12 23:49:24 +0300

    Correct output for multi-cxi input list

--- lst2cxi.py
+++ lst2cxi.py
@@ -11,7 +11,8 @@ import sys
 def lst2cxi(
     input_lst: str,
     cxi_path="/entry_1/data_1/data",
-    output_cxi=None,
+    output_cxi_prefix=None,
+    output_lst=None,
     compression="gzip",
     chunks=True,
 ) -> str:
@@ -21,7 +22,7 @@ def lst2cxi(
     Arguments:
         input_lst {str} -- input list (might be both with and without events)
         cxi_path {str} -- path of data inside cxi file to be copied
-        output_cxi {None} -- output cxi file. If 'None', will be <input_list>.cxi for <input_list>.lst
+        output_cxi_prefix {None} -- output cxi file prefix. If 'None', will be <input_list>.cxi for <input_list>_{1,2,3,...}.lst
 
     Returns:
         str -- filename of cxi file
@@ -48,30 +49,37 @@ def lst2cxi(
                     filename = os.getcwd() + filename
                 events_dict[line] = {None}
 
-    if output_cxi is None:
-        output_cxi = input_lst.rsplit(".")[0] + ".cxi"
+    if output_cxi_prefix is None:
+        output_cxi_prefix = ""
 
-    with h5py.File(output_cxi, "w") as h5fout:
-        for input_cxi, events in tqdm(
-            events_dict.items(), desc=f"Processing files in {input_lst} one by one"
-        ):
-            with h5py.File(input_cxi, "r") as h5fin:
+    cxi_cnt = 0
+    output_cxi_list = []
+    for input_cxi, events in tqdm(
+        events_dict.items(), desc=f"Processing files in {input_lst} one by one"
+    ):
+        cxi_cnt += 1
+        with h5py.File(input_cxi, "r") as h5fin:
+            data = h5fin[cxi_path]
+            if None in events:  # meaning we must dump whole cxi
                 data = h5fin[cxi_path]
-                if None in events:  # meaning we must dump whole cxi
-                    data = h5fin[cxi_path]
-                else:
-                    #  data = np.array(data.value)[np.array(list(events))]
-                    data = np.array(data[np.array(sorted(list(events)))])
-                shape = data.shape
+            else:
+                data = np.array(data[np.array(sorted(list(events)))])
+            shape = data.shape
+
+            output_cxi = f'{input_lst.rsplit(".")[0]}_{cxi_cnt}.cxi'
+            output_cxi_list.append(output_cxi)
+
+            with h5py.File(output_cxi, "w") as h5fout:
                 h5fout.create_dataset(
-                    cxi_path,
-                    shape,
-                    compression="gzip",
-                    data=data,
-                    chunks=chunks
+                    cxi_path, shape, compression="gzip", data=data, chunks=chunks
                 )
 
-    return output_cxi
+    # here we print updated lst
+    if output_lst is None:
+        output_lst = f'{input_lst.rsplit(".")[0]}_upd.lst'
+    with open(output_lst, "w") as fout:
+        print(*output_cxi_list, sep="\n", file=fout)
+    return output_lst
 
 
 def main(args):
@@ -89,7 +97,7 @@ def main(args):
         "--cxi_path", type=str, help="Path to your data inside a cxi file"
     )
     parser.add_argument(
-        "--output_cxi",
+        "--output_cxi_prefix",
         type=str,
         help="Output cxi file (if not present, will replace input *.lst to *.h5) ",
         default=None,
@@ -98,13 +106,17 @@ def main(args):
     args = parser.parse_args()
 
     if args.cxi_path is None:
-        ret = lst2cxi(input_lst=args.input_lst, output_cxi=args.output_cxi)
+        ret = lst2cxi(
+            input_lst=args.input_lst, output_cxi_prefix=args.output_cxi_prefix
+        )
     else:
         ret = lst2cxi(
-            input_lst=args.input_lst, cxi_path=args.cxi_path, output_cxi=args.output_cxi
+            input_lst=args.input_lst,
+            cxi_path=args.cxi_path,
+            output_cxi_prefix=args.output_cxi_prefix,
         )
 
-    print(f"Data successfully written to {ret}")
+    print(f"Updated list successfully written to {ret}")
 
 
 if __name__ == "__main__":
