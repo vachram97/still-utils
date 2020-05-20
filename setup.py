a209cfe0f3941916a6a5083df417dbf8cf4529a3
Egor Marin <marin@phystech.edu>
2020-05-20 15:34:22 +0300

    Change project structure

--- setup.py
+++ setup.py
@@ -2,3 +2,11 @@ from setuptools import setup
 
 setup(name="runner", scripts=["src/runner"])
 # pip3 install .
+
+# numpy==1.18.0
+# matplotlib==3.1.2
+# rmsd==1.3.2
+# h5py==2.10.0
+# scipy==1.4.1
+# tqdm==4.19.5
+# PyYAML==5.3.1
