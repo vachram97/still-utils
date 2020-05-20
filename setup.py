bc5afb9fe508dc3df71f172b609a59a2462639d8
Egor Marin <marin@phystech.edu>
2020-05-20 15:43:16 +0300

    First setup.py draft

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
+import glob
 
 setup(name="runner", scripts=["src/runner"])
 # pip3 install .
 
-# numpy==1.18.0
-# matplotlib==3.1.2
-# rmsd==1.3.2
-# h5py==2.10.0
-# scipy==1.4.1
-# tqdm==4.19.5
-# PyYAML==5.3.1
+requirements = """\
+numpy==1.18.0
+matplotlib==3.1.2
+rmsd==1.3.2
+h5py==2.10.0
+scipy==1.4.1
+tqdm==4.19.5
+PyYAML==5.3.1"""
+
+setup(
+    name="crystfeler",
+    version="0.1",
+    description="Module full of CLI tools to support your SSX/SFX data processing",
+    author="Egor Marin",
+    author_email="marin@phystech.edu",
+    packages=["crystfeler"],  # same as name
+    install_requires=requirements.split("\n"),  # external packages as dependencies
+    scripts=glob.glob("./src/crystfeler/sh/*er") + glob.glob("./src/crystfeler/py/*"),
+)
