56f7e0408bddad6118c0cbfc5902d83ba090cf1f
Egor Marin <marin@phystech.edu>
2020-08-13 18:32:53 +0300

    Add simple parameters to the file beginning

--- crystfeler/sh/scanmerge.sh
+++ crystfeler/sh/scanmerge.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+INPUT=$1
+SYMMETRY='mmm'
+HIGHRES="2.3"
+
 for pushres in $(python3 -c "print(*[i/10 for i in range(1,40,5)])"); do 
 	for method in process_hkl; do
-		for min_cc in -0.1 0.0 0.1 0.2; do
+		for min_cc in -1 -0.1 0.0 0.1 0.2; do
 			for string_params in ' ' '--scale'; do
-				analyzer -i laststream --merging "$method" --model "$model" --symmetry '2/m_uab' --iterations "$iterations" --lowres 30.0 --highres 2.6 --pushres "$pushres" --min_cc "$min_cc" --string_params "$string_params"
+				analyzer -i ${INPUT} --merging "$method" --model "$model" --symmetry ${SYMMETRY} --iterations "$iterations" --lowres 30.0 --highres ${HIGHRES} --pushres "$pushres" --min_cc "$min_cc" --string_params "$string_params"
 			done
 		done
 	done
@@ -14,7 +18,7 @@ for pushres in $(python3 -c "print(*[i/10 for i in range(1,40,5)])"); do
 	for method in partialator; do
 		for iterations in 0 1 2; do
 			for model in xsphere unity; do
-				analyzer -i laststream --merging "$method" --model "$model" --symmetry '2/m_uab' --iterations "$iterations" --lowres 30.0 --highres 2.6 --pushres "$pushres" --min_cc "$min_cc" --string_params "$string_params"
+				analyzer -i ${INPUT} --merging "$method" --model "$model" --symmetry ${SYMMETRY} --iterations "$iterations" --lowres 30.0 --highres ${HIGHRES} --pushres "$pushres" --min_cc "$min_cc" --string_params "$string_params"
 			done
 		done
 	done
