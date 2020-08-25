4c0f63a8f793bc50f699f6a750f71e993043a0ab
Egor Marin <marin@phystech.edu>
2020-08-25 20:26:34 +0300

    Rewrite denoiser to run on lst files; implement faster background and scalefactor determination

--- crystfeler/py/denoiser.py
+++ crystfeler/py/denoiser.py
@@ -5,30 +5,94 @@ import numpy as np
 import h5py
 import sys
 from tqdm import tqdm
+import os
 
 
-def load_cxi(input_cxi: str, datapath: str, top=None):
-    """Loads a cxi image as np array"""
+def denoise_lst(
+    input_lst: str,
+    cxi_path="/entry_1/data_1/data",
+    output_cxi_prefix=None,
+    output_lst=None,
+    compression="gzip",
+    chunks=True,
+    chunksize=100,
+    chunksize_threshold=50,
+    percentile=45,
+    zero_negative=True,
+) -> None:
 
-    if top is None:
-        top = int(1e10)
+    events_dict = {}
+    with open(input_lst, "r") as fin:
+        for line in fin:
+            if "//" in line:  # if line represents an event in cxi
+                filename, event = line.split(" //")
+                event = int(event)
+                if filename[0] != "/":  # if path is not absolute
+                    filename = f"{os.getcwd()}/{filename}"
+                if filename in events_dict:
+                    if None not in events_dict[filename]:
+                        events_dict[filename].add(event)
+                    else:
+                        pass
+                else:
+                    events_dict[filename] = {event}
+            else:  # if line represents both event and its absence
+                filename = line.rstrip()
+                if filename[0] != "/":  # if path is not absolute
+                    filename = os.getcwd() + filename
+                events_dict[line] = {None}
 
-    with h5py.File(input_cxi, "r") as f:
-        ret = np.array(f[datapath][:top])
+    if output_cxi_prefix is None:
+        output_cxi_prefix = ""
 
-    print(f"Loaded {input_cxi} sucessfully")
-    return ret
+    cxi_cnt = 0
+    output_cxi_list = []
+    for input_cxi, events in tqdm(
+        events_dict.items(), desc=f"Processing files in {input_lst} one by one"
+    ):
+        cxi_cnt += 1
+
+        # create events list
+        if None in events:
+            with h5py.File(input_cxi, "r") as fin:
+                cxi_fist_dimension = fin.shape[0]
+                events = list(range(cxi_fist_dimension))
+        else:
+            events = sorted(list(events))
+
+        num_images = len(events)
+
+        with h5py.File(input_cxi, "r") as h5fin:
+            data = h5fin[cxi_path]
+
+            for chunk_idx in range(0, num_images, chunksize):
+                start, stop = chunk_idx * chunksize, (chunk_idx + 1) * chunksize
+                current_data = data[events][start:stop]
+                if current_data.shape < chunksize_threshold:
+                    print(
+                        f"Some images from {input_cxi} won't be saved since their shape is less than {chunksize_threshold}"
+                    )
+                else:
+                    new_data = process_chunk(current_data, percentile=percentile)
+                    if zero_negative:
+                        new_data[new_data < 0] = 0
+
+                    output_cxi = f'{input_lst.rsplit(".")[0]}_{cxi_cnt}_{chunk_idx}.cxi'
+                    shape = current_data.shape
+
+                    with h5py.File(output_cxi, "w") as h5fout:
+                        h5fout.create_dataset(
+                            cxi_path,
+                            shape,
+                            compression="gzip",
+                            data=new_data,
+                            chunks=chunks,
+                        )
 
 
 def apply_mask(np_arr, center, r=45):
 
-    print("Started applying mask")
-    if len(np_arr.shape) == 3:
-        shape = np_arr.shape[1:]
-        shape_type = 3
-    else:
-        shape = np_arr.shape
-        shape_type = 2
+    shape = np_arr.shape[1:]
     mask = np.ones(shape)
 
     rx, ry = map(int, center)
@@ -37,60 +101,82 @@ def apply_mask(np_arr, center, r=45):
             if (x - rx) ** 2 + (y - ry) ** 2 <= r ** 2:
                 mask[x][y] = 0
 
-    print("Mask applied")
-    if shape_type == 2:
-        return np_arr * mask
-    else:
-        mask = mask.reshape((*shape, 1))
-        return np_arr * mask.reshape(1, *shape)
+    mask = mask.reshape((*shape, 1))
+    return np_arr * mask.reshape(1, *shape)
 
 
-def background(arr, q=90):
+def background(arr, q=45):
     """Return median of q-th percentile of each pixel"""
 
     print("Start background estimation")
-    p = np.percentile(arr, q=90, axis=(0))
-    b = np.median(arr * (arr < p), axis=0)
-
-    print("Background estimated")
-    return b
+    return np.percentile(arr, q=q, axis=(0))
 
 
-def scalefactors(arr, bkg, threshold=0.01, nsteps=100):
+def bin_scale(arr, b, alpha=0.01, num_iterations=10):
+    """
+    bin_scale binary search for proper scale factor
+
+    Parameters
+    ----------
+    arr : np.ndarray
+        Input 3-D array (N + 2D)
+    b : background
+        Single image (backgorund profile)
+    alpha : float, optional
+        Share of pixels to be negative, by default 0.01
+    num_iterations : int, optional
+        Number of binary search iterations, by default 10
+
+    Returns
+    -------
+    np.ndarray
+        proper scalefactors
+    """
+
+    num_negative = alpha * arr.shape[0] * arr.shape[1]
+
+    def count_negative(scale):
+        return (arr - scale * b < 0).sum()
+
+    l, r, m = 0, 1, 2
+
+    for _ in range(num_iterations):
+        m = (l + r) / 2
+        mv = count_negative(m)
+
+        if mv < num_negative:
+            l, r = m, r
+        else:
+            l, r = l, m
+
+    return l
+
+
+def scalefactors_bin(arr, bg, alpha=0.01, num_iterations=10):
     """\
     Find proper scalefactor for an image given a background 
     so that the share of negative pixels in resulting difference 
     is less thatn threshold
     """
-
-    print("Start scalefactor estimation")
-    scales = []
-    N = arr.shape[0]
-    n_pixels = arr.shape[1] * arr.shape[2]
-
-    for idx in tqdm(range(N), desc="Applying scalefactors"):
-        img = arr[idx]
-
-        x = np.linspace(0, 2, nsteps)
-        trials = [(img - bkg * scale < 0).sum() / n_pixels for scale in x]
-        scale = x[np.argmin(np.array(trials) < threshold)]
-
-        scales.append(scale)
-
-    print("Scalefactors estimated")
-    return np.array(scales)
+    return np.array(
+        [
+            bin_scale(arr[i], bg, alpha=alpha, num_iterations=num_iterations)
+            for i in range(arr.shape[0])
+        ]
+    )
 
 
-def output_arr(arr, datapath, filename="denoised.h5", dtype='int16'):
-    """Outputs array to cxi file"""
+def process_chunk(data, center=(720, 710), percentile=45):
+    data = apply_mask(data, center=center).astype(np.int16)
+    bg = background(data, q=percentile).astype(np.int16)
+    scales = scalefactors_bin(data, bg, alpha=5e-2)
 
-    print(f"Output fo {filename}...")
-    with h5py.File(filename, "w") as f:
-        if dtype is None:
-            f.create_dataset(datapath, data=arr)
-        else:
-            f.create_dataset(datapath, data=arr.astype(dtype))
+    full_bg = np.dot(bg.reshape(*(bg.shape), 1), scales.reshape(1, -1)).astype(np.int16)
+    full_bg = np.moveaxis(full_bg, 2, 0)
 
+    data = data - full_bg
+    del full_bg
+    return data
 
 
 def main(args):
@@ -100,7 +186,7 @@ def main(args):
         description="Denoises images using simple percentile filter"
     )
     parser.add_argument(
-        "input_cxi",
+        "input_lst",
         type=str,
         help="Input list in CrystFEL format (might be with or without events, or even combined)",
     )
@@ -108,46 +194,54 @@ def main(args):
         "--datapath", type=str, help="Path to your data inside a cxi file"
     )
     parser.add_argument("--center", type=str, help="Center position", default=None)
-
-    parser.add_argument("--percentile", type=int, help="Percentile to use", default=90)
+    parser.add_argument(
+        "--out_prefix", type=str, help="Prefix for output cxi files", default=None
+    )
+    parser.add_argument(
+        "--chunksize",
+        type=int,
+        help="Defines size of a single denoising chunk",
+        default=100,
+    )
+    parser.add_argument(
+        "--chunksize_threshold",
+        type=int,
+        help="Chunks less then that size wont be processed",
+        default=50,
+    )
+    parser.add_argument(
+        "--zero_negative",
+        type=bool,
+        help="Whether to zero out negative pixels or not",
+        default=True,
+    )
+    parser.add_argument(
+        "--out_prefix", type=str, help="Prefix for output cxi files", default=None
+    )
+    parser.add_argument("--percentile", type=int, help="Percentile to use", default=45)
     parser.add_argument(
         "--threshold",
         type=float,
         help="Threshold for scale factor determination",
-        default=0.01,
+        default=0.05,
     )
     parser.add_argument(
         "--radius", type=int, default=45, help="Radius to apply mask in the center"
     )
-    parser.add_argument(
-        "--top", type=int, default=100, help="Top N images to read from each cxi file"
-    )
-    parser.add_argument(
-        "--nsteps",
-        type=int,
-        default=200,
-        help="Number of points between 0 and 2 to try determine a scalefactor for each image",
-    )
 
     args = parser.parse_args()
 
-    arr = load_cxi(args.input_cxi, datapath=args.datapath, top=args.top)
     center = map(float, args.center.split())
     center = list(center)
-
-    arr_upd = apply_mask(arr, center)
-
-    bkg = background(arr_upd, q=args.percentile)
-    scales = scalefactors(arr_upd, bkg, threshold=args.threshold, nsteps=args.nsteps)
-    full_bkg = np.dot(bkg.reshape(*(bkg.shape), 1), scales.reshape(1, -1))
-    full_bkg = np.moveaxis(full_bkg, 2, 0)
-
-    denoised = arr_upd - full_bkg
-
-    denoised_filename = f"{'.'.join(args.input_cxi.rsplit('.'))}_denoised.cxi"
-    bkg_filename = f"{'.'.join(args.input_cxi.rsplit('.'))}_background.cxi"
-    output_arr(denoised, datapath=args.datapath, filename=denoised_filename)
-    output_arr(full_bkg, datapath=args.datapath, filename=bkg_filename)
+    denoise_lst(
+        input_lst,
+        cxi_path=args.datapath,
+        output_cxi_prefix=args.out_prefix,
+        chunksize=args.chunksize,
+        chunksize_theshold=args.chunksize_threshold,
+        zero_negative=args.zero_negative,
+        percentile=args.percentile,
+    )
 
 
 if __name__ == "__main__":
